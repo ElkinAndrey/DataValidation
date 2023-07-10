df24e6a7150e52915cad90283b231d2686859c06
ElkinAndrey <andreyridbir@gmail.com>
2023-07-10 14:35:48 +0500

    Добавил константы

---
+++ DataValidationAPI/DataValidationAPI.Domain/Constants/Policies.cs
@@ -0,0 +1,18 @@
+﻿namespace DataValidationAPI.Domain.Constants
+{
+    /// <summary>
+    /// Политики
+    /// </summary>
+    public class Policies
+    {
+        /// <summary>
+        /// Политика для менеджера и администратора
+        /// </summary>
+        public static readonly string Manager = "Manager";
+
+        /// <summary>
+        /// Политика только для администратора
+        /// </summary>
+        public static readonly string Admin = "Admin";
+    }
+}
---
+++ DataValidationAPI/DataValidationAPI.Domain/Constants/Roles.cs
@@ -0,0 +1,18 @@
+﻿namespace DataValidationAPI.Domain.Constants
+{
+    /// <summary>
+    /// Роли
+    /// </summary>
+    public class Roles
+    {
+        /// <summary>
+        /// Роль менеджера
+        /// </summary>
+        public static readonly string Manager = "Manager";
+
+        /// <summary>
+        /// Роль администратора
+        /// </summary>
+        public static readonly string Admin = "Admin";
+    }
+}
