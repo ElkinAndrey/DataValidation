ffed822a924c6c271fd24ef9c55dad5572735287
ElkinAndrey <andreyridbir@gmail.com>
2023-07-11 01:05:46 +0500

    Добавил интерфейс сервиса для работы с данными

---
+++ DataValidationAPI/DataValidationAPI.Service/Abstractions/IDataService.cs
@@ -0,0 +1,58 @@
+﻿using DataValidationAPI.Domain.Entities;
+
+namespace DataValidationAPI.Service.Services
+{
+    /// <summary>
+    /// Сервис для работы с данными
+    /// </summary>
+    public interface IDataService
+    {
+        /// <summary>
+        /// Получить срез всех данных
+        /// </summary>
+        /// <param name="start">Начало отчета</param>
+        /// <param name="length">Длина среза</param>
+        /// <param name="email">Часть электронной почты</param>
+        /// <param name="dateStart">Начало отчета даты</param>
+        /// <param name="dateEnd">Конец отчета даты</param>
+        /// <returns>Список со всеми данными</returns>
+        public Task<IEnumerable<Data>> GetDatasAsync(
+            int start = 0,
+            int length = int.MaxValue,
+            string? email = null,
+            DateTime? dateStart = null,
+            DateTime? dateEnd = null);
+
+        /// <summary>
+        /// Получить проверенных данных
+        /// </summary>
+        /// <param name="start">Начало отчета</param>
+        /// <param name="length">Длина среза</param>
+        /// <param name="email">Часть электронной почты</param>
+        /// <param name="dateStart">Начало отчета даты</param>
+        /// <param name="dateEnd">Конец отчета даты</param>
+        /// <returns>Список с проверенными данныхм</returns>
+        public Task<IEnumerable<Data>> GetValidDatasAsync(
+            int start,
+            int length,
+            int email,
+            DateTime dateStart,
+            DateTime dateEnd);
+
+        /// <summary>
+        /// Добавить не проверенные данные
+        /// </summary>
+        /// <param name="personId">Id человека, который оставил данные</param>
+        /// <param name="information">Информация</param>
+        public Task AddNoValidDatesAsync(Guid personId, string information);
+
+        /// <summary>
+        /// Выдать оценку данным
+        /// </summary>
+        /// <param name="dataId">Id данных</param>
+        /// <param name="valid">Как проверено</param>
+        public Task RateDataAsync(
+            Guid dataId,
+            bool? valid = null);
+    }
+}
