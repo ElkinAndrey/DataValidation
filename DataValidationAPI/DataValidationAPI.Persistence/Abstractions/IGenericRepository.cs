db78daaeb8d3083a40df56ba7a1abc79f5292c06
ElkinAndrey <andreyridbir@gmail.com>
2023-07-10 14:51:33 +0500

    Добавил интерфейс универсального репозитория

---
+++ DataValidationAPI/DataValidationAPI.Persistence/Abstractions/IGenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataValidationAPI.Domain.Entities;
+
+namespace DataValidationAPI.Persistence.Abstractions
+{
+    /// <summary>
+    /// Интерфейс универсального репозитория
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// Получить все
+        /// </summary>
+        /// <returns>Список сущностей</returns>
+        public Task<IQueryable<TEntity>> Get();
+
+        /// <summary>
+        /// Получить по Id
+        /// </summary>
+        /// <param name="id">Id сущности</param>
+        /// <returns>Сущность</returns>
+        public Task<TEntity> GetById(Guid id);
+
+        /// <summary>
+        /// Добавить
+        /// </summary>
+        /// <param name="entity">Добавляемая сущность</param>
+        public Task Insert(TEntity entity);
+
+        /// <summary>
+        /// Удалить по Id
+        /// </summary>
+        /// <param name="id">Id удаляемой сущности<</param>
+        public Task Delete(Guid id);
+
+        /// <summary>
+        /// Удалить сущность
+        /// </summary>
+        /// <param name="entity">Удаляемая сущность</param>
+        public Task Delete(TEntity entity);
+
+        /// <summary>
+        /// Изменить
+        /// </summary>
+        /// <param name="entity">Изменяемая сущность</param>
+        public Task Update(TEntity entity);
+    }
+}
