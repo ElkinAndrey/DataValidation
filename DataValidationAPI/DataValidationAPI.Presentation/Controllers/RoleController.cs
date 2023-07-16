29aa734982f06b7b4ad95ec1926034bd31e0a008
ElkinAndrey <andreyridbir@gmail.com>
2023-07-16 18:16:49 +0500

    Добавил конечные точки для работы с ролями

---
+++ DataValidationAPI/DataValidationAPI.Presentation/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataValidationAPI.Presentation.Controllers
+{
+    [Route("api/role")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        public RoleController() { }
+
+        [HttpGet]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetRolesAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{roleId}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetRoleByIdAsync(Guid roleId)
+        {
+            return Ok();
+        }
+    }
+}
